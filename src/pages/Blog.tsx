d9dc753d2c6d9fbedb33b18736a66be989fd4230
Anthonydevep01 <antonypac2000@gmail.com>
2026-03-03 21:59:51 -0600

    revert:restore-www-canonicals-and-redirects

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -107,7 +107,7 @@ export default function Blog() {
       seo={{
         title: "Blog TicoGPS | Noticias y Consejos de Seguridad",
         description: "Artículos sobre rastreo GPS, seguridad vehicular, mantenimiento de flotas y tecnología en Costa Rica.",
-        canonical: "https://ticogps.com/blog"
+        canonical: "https://www.ticogps.com/blog"
       }}
     >
       {!loading && posts.length > 0 && (
@@ -119,7 +119,7 @@ export default function Blog() {
               "itemListElement": posts.map((p, i) => ({
                 "@type": "ListItem",
                 "position": i + 1,
-                "url": `https://ticogps.com/blog/${p.slug}`
+                "url": `https://www.ticogps.com/blog/${p.slug}`
               }))
             })}
           </script>
