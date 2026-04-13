0f479b40416e5a19c6523bd445f81e8446d5f2d5
Anthonydevep01 <antonypac2000@gmail.com>
2026-04-12 21:52:51 -0600

    deploy_update_gps_4g_price_to_80000

--- src/data/products.ts
+++ src/data/products.ts
@@ -186,7 +186,7 @@ export const products: Product[] = [
     id: "gps-basico-vehiculo",
     name: "GPS 4G para vehiculo",
     description: "Solución de rastreo esencial para vehículos particulares y pequeñas flotas. Monitoreo confiable y funciones de seguridad estándar.",
-    price: "₡75,000",
+    price: "₡80,000",
     images: ["/images/TicoGPS-GPS02.jpeg", "/images/TicoGPS-GPS03.jpeg"],
     category: ["GPS"],
     brand: "TicoGPS",
